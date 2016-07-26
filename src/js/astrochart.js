0fa5453f86366f0383029871da908f17a7093801
Bharata <ninguem@nenhur.es>
2016-07-26 18:13:46 -0300

    Adds houses.

--- src/js/astrochart.js
+++ src/js/astrochart.js
@@ -9,7 +9,21 @@ window.Astrochart = (function(w, h) {
 
     var now = {
         'ascendant': 0,
-        'houses': [],
+        'mc': 270,
+        'houses': { 
+            '1' : 30,
+            '2' : 60,
+            '3' : 90,
+            '4' : 120,
+            '5' : 150,
+            '6' : 180,
+            '7' : 210,
+            '8' : 240,
+            '9' : 270,
+            '10' : 300,
+            '11' : 330,
+            '12' : 360
+        },
         'planets': {
             'sun': 0,
             'mercury': 0, 
@@ -38,6 +52,9 @@ window.Astrochart = (function(w, h) {
 
             // Creates the orbit for space objects
             orbit = createOrbitPath(w/2, h/2, 230);
+
+            var houses = svg.select("g#houses");
+            snap.append(houses);
         });
 
         Snap.load("/dist/image/things.svg", function(svg) {
@@ -108,9 +125,10 @@ window.Astrochart = (function(w, h) {
                       "m -{r}, 0 " +
                       "a {r},{r} 0 1,0 {d},0 " + 
                       "a {r},{r} 0 1,0 -{d},0").format({cx:cx, cy:cy, r:r, d:r*2}));
-        path.attr('fill', 'none');
-        path.attr('stroke', 'red');
-        path.attr('stroke-dasharray', '1,1');
+        path.attr({
+            'id': 'orbit',
+            'fill': 'none'
+        });
 
         return path;
     };
@@ -133,15 +151,7 @@ window.Astrochart = (function(w, h) {
     };
 
     function move(planets, degrees) {
-        if (typeof planets === "object") {
-            for (var planet in planets) {
-                _move(planet, planets[planet]);
-            }
-            return this;
-
-        } else if (typeof planets === "string") {
-            return _move(planets, degrees);
-        }
+        return iterateIfCollection(planets, degrees, _move);
     }
 
     function _move(planet, degrees) {
@@ -166,15 +176,73 @@ window.Astrochart = (function(w, h) {
         }
     }
 
+    function house(houses, degrees) {
+        return iterateIfCollection(houses, degrees, _house);
+    }
+
+    function _house(house, degrees) {
+        if (now.houses[house] === undefined) {
+            return false;
+        }
+
+        if (degrees !== undefined) {
+            var houseElements = { 
+                '1': { id: 'house-1' },
+                '2': { id: 'house-2' },
+                '3': { id: 'mc' },
+                '4': { id: 'house-4' },
+                '5': { id: 'house-5' },
+                '6': { }, // Descendant is fixed
+                '7': { id: 'house-1' },
+                '8': { id: 'house-2' },
+                '9': { id: 'mc' },
+                '10': { id: 'house-4' },
+                '11': { id: 'house-5' },
+                '12': { } // Ascendant is fixed
+            }
+
+            var element = houseElements[house].id !== undefined ? 
+                            snap.select('#' + houseElements[house].id) : null;
+            if (element) {
+                var matrix = new Snap.Matrix()
+                matrix.rotate(now.houses[house] - degrees, 300, 300);
+                matrix.add(element.transform().localMatrix);
+                element.transform(matrix);
+
+                console.log(matrix.toTransformString());
+            }
+            
+            now.houses[house] = degrees;
+            return this;
+
+        } else {
+            return now.houses[house];
+        }
+    }
+
+    function iterateIfCollection(argument, parameter, callback) {
+        if (typeof argument === "object") {
+            for (var i in argument) {
+                callback(i, argument[i]);
+            }
+            return this;
+
+        } else if (typeof argument === "string") {
+            return callback(argument, parameter);
+        }
+
+    }
+
     // Initialize this instance and return public API.
     _Astrochart(w !== undefined ? w : 600, 
                 h !== undefined ? h : 600);
 
     return {
+        snap: snap,
         version: version,
         ascendant: ascendant,
         move: move,
-        snap: snap
+        house: house
     };
 
 });
