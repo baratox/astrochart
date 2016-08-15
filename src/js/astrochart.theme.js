06beeee97a17f030042764e58e1602a674f2a82f
Bharata <nandorf@gmail.com>
2016-08-15 18:54:12 -0300

    Refactors AstrochartTheme to use the same declaration style as Astrochart. Why the hell was it different anyway???

--- src/js/astrochart.theme.js
+++ src/js/astrochart.theme.js
@@ -1,7 +1,5 @@
-Astrochart.AstrochartTheme = function(svg) {
-    this.svg = svg;
-    
-    this.rotation = {
+Astrochart.AstrochartTheme = function(_svg) {
+    var _rotation = {
         'zodiac': 105,
         'houses': { '1' : 30, '2' : 60, '3' : 90, '4' : 120, '5' : 150, '6' : 180,
                     '7' : 210, '8' : 240, '9' : 270, '10' : 300, '11' : 330, '12' : 360 },
@@ -21,7 +19,7 @@ Astrochart.AstrochartTheme = function(svg) {
         }
     };
 
-    this.houses = { 
+    var _houses = { 
         '1': { text: 'house-1-text', id: 'house-1', },
         '2': { text: 'house-2-text', id: 'house-2' },
         '3': { text: 'house-3-text', id: 'mc' },
@@ -42,19 +40,19 @@ Astrochart.AstrochartTheme = function(svg) {
      * - zodiac: Zodiac-originated angle, with 0º at the 
      *           start of Aries.
      */
-    this._rotate = function(zodiac) {
-        return zodiac - this.rotation.zodiac;
+    var _rotate = function(zodiac) {
+        return zodiac - _rotation.zodiac;
     };
 
-    this._centerHouseText = function(house) {
-        var element = this.svg.select('#' + this.houses[house].text);
+    var _centerHouseText = function(house) {
+        var element = _svg.select('#' + _houses[house].text);
         if (element) {
             var next = parseInt(house) < 12 ? parseInt(house) + 1 : 1;
-            var center = this.rotation.houses[house] + (this.rotation.houses[next] - this.rotation.houses[house]) / 2;
+            var center = _rotation.houses[house] + (_rotation.houses[next] - _rotation.houses[house]) / 2;
             console.log("Centering text for house", house, "to", center, next);
 
-            var rotation = center - this.rotation["house-texts"][house];
-            this.rotation["house-texts"][house] = center;
+            var rotation = center - _rotation["house-texts"][house];
+            _rotation["house-texts"][house] = center;
 
             var matrix = new Snap.Matrix();
             matrix.rotate(rotation, 300, 300);
@@ -65,65 +63,62 @@ Astrochart.AstrochartTheme = function(svg) {
             console.warn("No text for house", house);
         }
     };
-};
 
-Astrochart.AstrochartTheme.prototype = {
     /**
      * Rotates the wheel so that given angle 'zodiac' is at 180º.
      * - zodiac: Zodiac-originated angle, with 0º at the 
      *           start of Aries.
      */
-    "ascendant": function(zodiac) {
+    var ascendant = function(zodiac) {
         // Sprite is rotated 105º clockwise
-        var angleFrom = this.rotation.zodiac - 105;
+        var angleFrom = _rotation.zodiac - 105;
         var angleTo = zodiac - 105;
 
         console.debug("Rotating zodiac from", angleFrom, "to", angleTo);
 
-
         if (angleFrom != angleTo) {
-            var wheel = this.svg.select("g#zodiac");
+            var wheel = _svg.select("g#zodiac");
             if (wheel) {
                 // TODO Rotate everything else too
                 Snap.animate(angleFrom, angleTo, function(value) {
                     wheel.transform("r" + value + ",300,300");
                 }, 800);
                 
-                this.rotation.zodiac = zodiac;
+                _rotation.zodiac = zodiac;
             }
         }
-    },
+    };
 
-    "house": function(house, zodiac) {
-        var fixed = this._rotate(zodiac);
-        if (this.houses[house].id !== undefined) {
-            var rotation = this.rotation['houses'][house] - fixed;
+    var house = function(house, zodiac) {
+        var fixed = _rotate(zodiac);
+        if (_houses[house].id !== undefined) {
+            var rotation = _rotation['houses'][house] - fixed;
             console.debug("Rotating house", house, 'to', zodiac, " (", fixed, "). Rotation:", rotation);
 
-            var element = this.svg.select('#' + this.houses[house].id);
+            var element = _svg.select('#' + _houses[house].id);
             if (element) {
                 var matrix = new Snap.Matrix();
                 matrix.rotate(rotation, 300, 300);
                 matrix.add(element.transform().localMatrix);
                 element.transform(matrix);
 
-                this._centerHouseText(house);
+                _centerHouseText(house);
 
-                this.rotation.houses[house] = fixed;
+                _rotation.houses[house] = fixed;
             } else {
                 console.warn("Nothing to move");
             }
         }
 
-    },
+    };
 
-    "astro": function(name, zodiac) {
-        var angleFrom = this.rotation.planets[name],
-            angleTo = this._rotate(zodiac);
+    var astro = function(name, zodiac) {
+        var angleFrom = _rotation.planets[name],
+            angleTo = _rotate(zodiac);
         
         console.debug("Moving", name, "from", angleFrom, "to", angleTo);
 
-        var element = this.svg.select("g#" + name);
+        var element = _svg.select("g#" + name);
         if (element) {
             if (angleFrom < 0) { angleFrom = 360 + angleFrom; }
             if (angleTo < 0) { angleTo = 360 + angleTo; }
@@ -133,8 +128,14 @@ Astrochart.AstrochartTheme.prototype = {
                     element.orbit(orbit, value);
                 }, 400);
 
-            this.rotation.planets[name] = angleTo;
+            _rotation.planets[name] = angleTo;
         }
+    };
 
+    // Builds the public API for an AstrochartTheme.
+    return {
+        "ascendant" : ascendant,
+        "astro" : astro,
+        "house" : house
     }
 };
