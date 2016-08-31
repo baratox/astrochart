028338b0045d9d2b13489c8252640bdbfd65b629
Bharata <ninguem@nenhur.es>
2016-08-31 17:28:38 -0300

    Fix transformOriginal when there is no original transform.

--- src/js/snap/snap.transformOriginal.js
+++ src/js/snap/snap.transformOriginal.js
@@ -9,22 +9,27 @@ Snap.plugin(function(Snap, Element, Paper) {
         } else {
             // Prepends t to the original transform by default
             append = typeof append !== "undefined" ? append : false;
-            var original = this.data("original-transform").clone();
-            if (t instanceof Snap.Matrix) {
-                if (append) {
-                    this.transform(original.add(t));
-                } else {
-                    this.transform(t.add(original));
-                }
+            var original = this.data("original-transform") != null ? this.data("original-transform").clone() : null;
+            if (original) {
+                if (t instanceof Snap.Matrix) {
+                    if (append) {
+                        this.transform(original.add(t));
+                    } else {
+                        this.transform(t.add(original));
+                    }
 
-            } else {
-                if (append) {
-                    this.transform(original);
-                    this.transform(t);
                 } else {
-                    this.transform(t);
-                    this.transform(original);
+                    if (append) {
+                        this.transform(original);
+                        this.transform(t);
+                    } else {
+                        this.transform(t);
+                        this.transform(original);
+                    }
                 }
+                
+            } else {
+                this.transform(t);
             }
         }
     };
